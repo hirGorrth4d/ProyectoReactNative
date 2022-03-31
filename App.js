6568d311b5db17d4823731c1aa4f4010ff04f290
sonovdragons <cagliolom@gmail.com>
2022-03-30 22:31:24 -0300

    componentes y listas

--- App.js
+++ App.js
@@ -1,45 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View, ScrollView } from 'react-native';
-import React, { useState} from 'react';
+import React, { useState } from 'react';
+import { 
+  Text, 
+  View, 
+  FlatList, 
+} from 'react-native';
+import ModalItem  from './components/Modal';
+import AddItem from './components/AddItem';
+import styles from './styles';
 
-export default function App() {
-  const [counter, setCounter] = useState(0);
-  const [ textItem, setTextItem] = useState('');
-  const [listItem, setListItem ] = useState([{id:1,value:'Juan'},{id:2,value:'Martin'}]);
-  const onHandlerChangeText = (texto) => setTextItem(texto)
-  const addItem = () => {
-    setListItem(currentItems => [...currentItems, {id: Math.random.toString(), value: textItem}])
-    setTextItem('');
-    setCounter(counter + 1)
+export default function App() {  
+  const [ counter, setCounter ] = useState(3);
+  const [ listItem, setListItem ] = useState([{id:1, value:'Juan'},{id:2, value:'Pedro'}]);
+  const [ itemSelected, setItemSelected ] = useState({});
+  const [ modalVisible, setModalVisible ] = useState(false);
+
+  
+  const onHandlerDelete = id => { 
+      console.log("Item " + itemSelected.value + " Eliminado");
+      setListItem( currenItems => currenItems.filter( item => item.id !== id ));
+      setItemSelected({});
+      setModalVisible(!modalVisible);
+  }
+  const onHandlerModal = id => {
+    setItemSelected(listItem.filter( item => item.id === id)[0]);
+    setModalVisible(!modalVisible);
+  }  
+  const closeModal = () => {
+    setModalVisible(!modalVisible);
   }
 
+  const agregarItem = (textItem) => {   
+    console.log("Se Agrega.");
+    if(textItem!=="") {
+      console.log("Se agrego el item: " + textItem);
+      setListItem( currenItems => 
+        [...currenItems, {id: counter, value: textItem}]        
+      )
+      setCounter(counter + 1);
+    }
+  }
+  
+  const renderItem = data => 
+      <Text 
+        style={styles.listItem} 
+        onPress={onHandlerModal.bind(this, data.item.id)}>
+          * {data.item.value} ({data.item.id})
+      </Text>
 
   return (
-    <View style={styles.container}> 
-      <View style={{backgroundColor: "#DDDDDD", height: 200, width: 200, justifyContent: 'center'}}>
-        <TextInput 
-          placeholder='Ingrese su producto'
-          value={textItem}
-          onChangeText={onHandlerChangeText}/>
-        <Button title="Add" onClick={addItem}/>
+    <View style={styles.container}>
+      <AddItem onAddItem={agregarItem}/>      
+      <View style={styles.listItemContainer}>
+        <FlatList
+          data={listItem}
+          renderItem={renderItem}
+          keyExtractor={ item => item.id }
+        />
       </View>
-
-      <View style={{backgroundColor: "green", height: 200, width: 200, justifyContent: 'center'}}>
-        <ScrollView>{listItem.map(item => <Texto key={item.id} value={item.value}></Texto>)}</ScrollView>
-      </View>
-      
+      <ModalItem onDelete={onHandlerDelete} item={itemSelected} visible={modalVisible} onCancel={closeModal}/>
     </View>
   );
 }
-function Texto(props) {
-  return <Text>{props.value</Text>
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
